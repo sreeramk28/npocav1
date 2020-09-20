1754ff6b208f3e903c69d099be2f96d8fe8ef641
Sreeram K <sreeramfireball@gmail.com>
2020-09-20 13:44:41 +0530

    one footer change

--- includes/footer.php
+++ includes/footer.php
@@ -12,7 +12,7 @@
 		</table>
 		<br>
 		<p class="opensans-font footer-text">
-			NPOCA Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			NPOCA helps in careers sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat.
