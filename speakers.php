48f423cf807c17611862adb3064602a0e36dfbfc
Sreeram K <sreeramfireball@gmail.com>
2020-11-10 20:12:24 +0530

    added empty speakers page

---
+++ speakers.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php require_once("includes/bootstrap4.php"); ?>
+	<link rel="icon" type="image/jpeg" href="assets/favi.jpeg"/>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
+
+	<link href="style/navbar.css" rel="stylesheet">
+	<link href="style/footer.css" rel="stylesheet">
+	<link href="style/fontstyles.css" rel="stylesheet">
+
+	<script src="js/navbar.js" type="text/javascript"></script>
+</head>
+<body>
+	<!--header -->
+	<?php require_once("includes/navbar.php"); ?>
+	<div class="page-wrapper">
+		
+	</div>
+	
+	<br><br>
+	
+	<?php require_once("includes/footersmall.php") ?>
+	<!-- footer -->
+</body>
+</html>
