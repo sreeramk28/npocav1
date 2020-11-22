87655670fde4273eab1adab4fad2fc037ddfd593
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2020-11-22 20:05:00 +0530

    recordings page added

---
+++ recordings.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Recordings</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <?php require_once("includes/bootstrap4.php"); ?>
+    <link rel="icon" type="image/jpeg" href="assets/favi.jpeg" />
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    <link href="style/navbar.css" rel="stylesheet">
+    <link href="style/footer.css" rel="stylesheet">
+    <link href="style/fontstyles.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/recordings.css">
+</head>
+
+<body>
+    <!--header -->
+    <?php require_once("includes/navbar.php"); ?>
+    <div class="page-wrapper opensans-font">
+        <p class="ftsz-26 mont-font bold-emphasis text-center">Recordings</p>
+        <p class="ftsz-16 desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga accusantium totam
+            delectus
+            perspiciatis vitae saepe rem sapiente omnis dolorum a laudantium, magnam consequuntur esse officia! Delectus
+            nihil ex dignissimos nisi.</p>
+
+        <ul class="recordings  text-center my-4">
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/8_MU9CGSp48" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/0KPf4N66Phk" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="25" viewBox="0 0 18 25">
+                                <defs>
+                                    <style>
+                                        .a{fill:none;}.b,.c{stroke:none;}.c{fill:#707070;}
+                                    </style>
+                                </defs>
+                                <g class="a">
+                                    <path class="b" d="M1,0H17a1,1,0,0,1,1,1V24a1,1,0,0,1-1,1L9.157,17,1,25a1,1,0,0,1-1-1V1A1,1,0,0,1,1,0Z" />
+                                    <path class="c" d="M 1 1 L 1 23.59934043884277 L 8.456950187683105 16.28604888916016 L 9.171039581298828 15.58572006225586 L 9.87123966217041 16.29994010925293 L 17 23.57154083251953 L 17 1 L 1 1 M 1 0 L 17 0 C 17.55228042602539 0 18 0.4477195739746094 18 1 L 18 24 C 18 24.55228042602539 17.55228042602539 25 17 25 L 9.157150268554688 17 L 1 25 C 0.4477195739746094 25 0 24.55228042602539 0 24 L 0 1 C 0 0.4477195739746094 0.4477195739746094 0 1 0 Z" />
+                                </g>
+                            </svg>
+                        </span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/K2luea-MBRw" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/8_MU9CGSp48" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/0KPf4N66Phk" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/K2luea-MBRw" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/8_MU9CGSp48" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/0KPf4N66Phk" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="card">
+                <div class="content-box">
+                    <div class="iframe">
+                        <iframe src="https://www.youtube.com/embed/K2luea-MBRw" width="268" height="170" frameborder="0"></iframe>
+                    </div>
+                    <div class="detail px-2 pt-2">
+                        <p class="time ftsz-13 mx-0"><span class="calander"><img src="./assets/recordings-page/calendar.svg" alt=""
+                                    srcset=""></span>Sep 13, Sunday <span class="duration ftsz-13">20 min</span></p>
+                    </div>
+                    <p class="title px-2 mt-2 mont-font semibold-emphasis ftsz-20">Lorem ipsum dolor sit amet,</p>
+                    <div class="footer px-2 pb-2">
+                        <p class="author ftsz-13">Rahul Kumar <span class="save"><img src="./assets/recordings-page/Save icon.svg"
+                                    alt=""></span></p>
+                    </div>
+                </div>
+            </li>
+    </div>
+    </div>
+    <!-- footer -->
+    <?php require_once("includes/footerlgall.php") ?>
+    <!-- Script  -->
+    <script src="js/navbar.js" type="text/javascript"></script>
+</body>
+
+</html>
